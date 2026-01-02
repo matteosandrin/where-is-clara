5ec069006b8c994f4f62f1af04da579410c10c27
Matteo Sandrin <sandrin98@gmail.com>
2026-01-02 17:38:14 -0500

    Add range query to backend api

--- backend/app/routers/position.py
+++ backend/app/routers/position.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
@@ -12,12 +13,12 @@ router = APIRouter(
 )
 
 
-@router.get(f"/")
+@router.get(f"/latest")
 async def get_latest_position_default_vessel(db: Session = Depends(get_db)):
     return await get_latest_position(settings.vessel_mmsi, db)
 
 
-@router.get(f"/<mmsi>")
+@router.get(f"/latest/<mmsi>")
 async def get_latest_position(mmsi: str, db: Session = Depends(get_db)):
     position = (
         db.query(Position)
@@ -28,3 +29,32 @@ async def get_latest_position(mmsi: str, db: Session = Depends(get_db)):
     if not position:
         raise HTTPException(status_code=404, detail="Position not found")
     return position
+
+
+@router.get(f"/range")
+async def get_positions_in_range_default_vessel(
+    from_ts: datetime | None = None,
+    to_ts: datetime | None = None,
+    db: Session = Depends(get_db),
+):
+    return await get_positions_in_range(settings.vessel_mmsi, from_ts, to_ts, db)
+
+
+@router.get(f"/range/<mmsi>")
+async def get_positions_in_range(
+    mmsi: str,
+    from_ts: datetime | None = None,
+    to_ts: datetime | None = None,
+    db: Session = Depends(get_db),
+):
+    positions = (
+        db.query(Position)
+        .filter(Position.mmsi == mmsi)
+        .filter(Position.timestamp >= from_ts if from_ts else True)
+        .filter(Position.timestamp <= to_ts if to_ts else True)
+        .order_by(Position.timestamp.desc())
+        .all()
+    )
+    if not positions:
+        raise HTTPException(status_code=404, detail="Positions not found")
+    return positions
